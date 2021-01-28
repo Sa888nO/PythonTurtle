aac69d769c1170f8c856e8102ba96475dc2efe94
Egorushkin A.S <egorushkin.a.s@mail.ru>
2021-01-28 16:00:56 +0300

    1stTask

---
+++ 1stTaskTurtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.color('green')
+for x in range(2):
+	turtle.forward(50)
+	turtle.left(90)
+for x in range(2):
+	turtle.forward(50)
+	turtle.right(90)
+turtle.forward(50)
+turtle.done()
